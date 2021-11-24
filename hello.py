0887e935179df05243de300c9b630a391231ccb8
Justin Franecki <jfranecki74@gmail.com>
2021-11-23 18:22:35 -0600

    third commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="Hello World!") 
+
+# Putting our Widget on the screen
+myLabel.pack()
+
+root.mainloop()
